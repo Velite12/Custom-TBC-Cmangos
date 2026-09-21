7c827b1ff96df0da6175c15d8e1b2025e43600e9
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-09-20 23:39:00 -0400

    Update Object.cpp

--- src/game/Entities/Object.cpp
+++ src/game/Entities/Object.cpp
@@ -3252,7 +3252,7 @@ int32 WorldObject::CalculateSpellEffectValue(Unit const* target, SpellEntry cons
             if (casterCLS && spellCLS)
             {
                 float CLSPowerCreature = casterCLS->BaseDamage;
-                float CLSPowerSpell = spellProto->spellLevel > spellCLS->BaseDamage ? spellProto->spellLevel : spellCLS->BaseDamage;
+                float CLSPowerSpell = spellCLS->BaseDamage;
                 value = value * (CLSPowerCreature / CLSPowerSpell);
             }
         }
